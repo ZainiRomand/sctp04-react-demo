8689fb28faff8671e22c929954c292d9d8371da5
Zaini Romand <zainiromand@gmail.com>
2025-02-27 13:41:13 +0000

    Added mutator function

--- 03-events-and-state/src/App.jsx
+++ 03-events-and-state/src/App.jsx
@@ -1,28 +1,44 @@
+import { useState } from 'react'
+
 export default function App() {
 
+  const [count, setCount] = useState(100);
+  const [colour, setColour] = useState("blue");
+
   const boxClicked = () => {
-    alert("The box has been clicked")
+    setCount(count + 1),
+    console.log("The box has been clicked")
   }
 
   const helloMouse = () => {
     alert("Mouse cursor detected")
   }
 
-return (
-  <>
-    <h1>Events</h1>
-    <div style= {{
-      "width":"50px",
-      "height":"50px",
-      "border":"1px solid black"
-    }} 
-    onClick={boxClicked}
-    onMouseEnter={helloMouse}
-    ></div>
-  
-  
-  </>
-)
+  return (
+    <>
+      <h1>Events</h1>
+      <div style={{
+        "width": "50px",
+        "height": "50px",
+        "border": "1px solid black",
+        "backgroundColor": colour,
+        "color": "white"
+      }}
+        onClick={boxClicked}
+        onMouseEnter={() => {
+          setColour( "red"),
+          console.log("Mouse enters the box")
+        }}
+        onMouseLeave={() => {
+          setColour( "blue"),
+          console.log("Mouse out of the box")
+        }}
+
+      >
+        {count}
+      </div>
+    </>
+  )
 
 
 }
