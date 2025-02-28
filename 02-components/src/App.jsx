efcc0dcb4eb52f6cdd97d97ebbef24247e82155e
Zaini Romand <zainiromand@gmail.com>
2025-02-28 01:24:20 +0000

    Added BorderImageFrame component

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,12 +1,21 @@
 
 import Alert from "./Alert"
+import apples from "./assets/apples.jpg"
+import oranges from "./assets/oranges.jpg"
+import cherries from "./assets/cherries.jpg"
+import kiwis from "./assets/kiwis.jpg"
 
 
 export default function App() {
 
-
   function sayHello() {
     return <h2>Hello How Are You?</h2>
+  }
+
+  function BorderImageFrame(props) {
+    return (
+      <img src={props.picture}/>
+    )
   }
 
   return (
@@ -20,6 +29,10 @@ export default function App() {
       <Alert message="Danger ahead" bgcolor="pink"/>
       <Alert message="Withdrawal amount exceeds bank account's balance" bgcolor="orange"/>
       <Alert message="Download successful" bgcolor="lightgreen"/>
+      <BorderImageFrame picture={apples}/>
+      <BorderImageFrame picture={oranges}/>
+      <BorderImageFrame picture={cherries}/>
+      <BorderImageFrame picture={kiwis}/>
    </>
   )
 }
