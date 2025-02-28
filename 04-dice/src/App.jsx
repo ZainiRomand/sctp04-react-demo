67565a44becc4e0d2f2fa08eeb019a37ef4fb09e
Zaini Romand <zainiromand@gmail.com>
2025-02-28 15:29:16 +0000

    Added colour number

--- 04-dice/src/App.jsx
+++ 04-dice/src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
   let randomNumber = 1
 
   const [number, setNumber] = useState(randomNumber)
+  const [colour, setColour] = useState("red")
 
   return (
     <>
@@ -13,11 +14,18 @@ export default function App() {
         "width": "50px",
         "height": "50px",
         "border": "1px solid black",
-        "textAlign": "center"
+        "textAlign": "center",
+        "color": colour
       }}
         onClick={() => {
           const randomNumber = Math.floor(Math.random() * 6) + 1;
           setNumber(number => number = randomNumber);
+          if( randomNumber == 1)
+            setColour(colour => colour = "red")
+          else if (randomNumber == 6)
+            setColour(colour => colour = "green")
+          else
+            setColour(colour => colour = "black")
         }}
       >
         <br/>
